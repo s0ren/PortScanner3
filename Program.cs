628761350eb9bd76edaa31a430231c29c4f60d57
Søren Magnusson <smag@tec.dk>
2020-07-30 12:13:56 +0200

    Mere farver

--- Program.cs
+++ Program.cs
@@ -10,8 +10,11 @@ namespace PortScanner3
     {
         static void Main(string[] args)
         {
-            Console.BackgroundColor(ConsoleColor.DarkRed);
+            Console.BackgroundColor = ConsoleColor.DarkRed;
+            Console.ForegroundColor = ConsoleColor.White;
             Console.Write("Hej mormor!");
+            Console.BackgroundColor = ConsoleColor.DarkBlue;
+            Console.ForegroundColor = ConsoleColor.Yellow;
             Console.WriteLine("Hej Git'te-mor");
         }
     }
